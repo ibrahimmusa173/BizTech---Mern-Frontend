3b5783256af13906f47a42a17585d5fd51fdf27b
Musa Aman <musaaman705@gmail.com>
2026-06-17 15:12:27 +0500

    Revert "update localhost"
    This reverts commit 5dcb31318949b69e1bd2680393ab6b6104d2d56d.

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,17 +2,8 @@ import axios from 'axios';
 import { store } from '../store';
 import { setLogout } from '../store/authSlice';
 
-// 1. Detect if the app is running locally or on Vercel
-const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-
-// 2. Set the Base URL based on the environment
-// Change '5000' to whatever port your backend runs on locally (e.g., 8000 or 5000)
-const baseURL = isLocalhost 
-  ? 'http://localhost:7000/api' 
-  : `${import.meta.env.VITE_API_URL}/api`;
-
 const API = axios.create({
-  baseURL: baseURL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 API.interceptors.request.use((req) => {
